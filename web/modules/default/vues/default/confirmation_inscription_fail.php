f2ca51e1000f4f371991b8f7bfb60ea13be2b2f5
mfiari <fiarimike@yahoo.fr>
2016-10-23 11:41:47 +0200

    suppression de fichier inutile

--- web/modules/default/vues/default/confirmation_inscription_fail.php
+++
@@ -1,9 +0,0 @@
-<div class="row">
-	<div class="col-md-10  col-md-offset-1">
-		<h2>Confirmation Inscription échoué</h2>
-		<div>
-			Votre compte n'a pas pu être activé.<br />
-			<a>Renvoyer le mail de confirmation</a>
-		</div>
-	</div>
-</div>
