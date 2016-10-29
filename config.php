cfbc7c81a7d2d88a628f3152f3847dac6e370edd
mfiari <fiarimike@yahoo.fr>
2016-10-29 10:31:02 +0200

    modification configuration

--- config.php
+++ config.php
@@ -3,16 +3,16 @@
 	DEFINE ("ENVIRONNEMENT", "DEV");
 
 	/* path config */
-	DEFINE ("ROOT_PATH", "D:/wamp/www/projets/fiche_partage/website/");
+	DEFINE ("ROOT_PATH", "D:/wamp/www/projets/fiche_partage/");
 	DEFINE ("MODEL_PATH", ROOT_PATH."models/");
 	DEFINE ("WEBSITE_PATH", ROOT_PATH."web/");
 	DEFINE ("WEBSERVICES_PATH", ROOT_PATH."webservices/");
 	
 	/* WEB rootes */
-	DEFINE ("WEBSITE_URL", "http://localhost/projets/fiche_partage/website/web/");
+	DEFINE ("WEBSITE_URL", "http://localhost/projets/fiche_partage/web/");
 
 	/* Webservices config */
-	DEFINE ("WS_URL", "http://localhost/projets/fiche_partage/website/webservice/");
+	DEFINE ("WS_URL", "http://localhost/projets/fiche_partage/webservice/");
 	DEFINE ("WS_AUTH_LOGIN", "");
 	DEFINE ("WS_AUTH_PASSWORD", "");
 	
@@ -42,10 +42,28 @@
 	DEFINE ("LOG_LEVEL_ERROR", "ERROR");
 	
 	/* Constante */
-	DEFINE ("USER_CLIENT", "USER");
-	DEFINE ("USER_LIVREUR", "LIVREUR");
-	DEFINE ("USER_RESTAURANT", "RESTAURANT");
+	DEFINE ("USER_CLASSIQUE", "CLASSIQUE");
+	DEFINE ("USER_PRO", "PRO");
+	DEFINE ("USER_PREMIUM", "PREMIUM");
 	DEFINE ("USER_ADMIN", "ADMIN");
-	DEFINE ("USER_ADMIN_RESTAURANT", "ADMIN_RESTAURANT");
+	
+	/* MAIL */
+	DEFINE ("SEND_MAIL", false);
+	DEFINE ("MAIL_FROM_DEFAULT", "test@homemenus.fr");
+	DEFINE ("MAIL_CONTACT", "test@homemenus.fr");
+	DEFINE ("MAIL_RESTAURANT", "test@homemenus.fr");
+	DEFINE ("MAIL_LIVREUR", "test@homemenus.fr");
+	DEFINE ("MAIL_PRO", "test@homemenus.fr");
+	DEFINE ("MAIL_ADMIN", "test@homemenus.fr");
+	
+	/* PAYPAL */
+	DEFINE ("PAYPAL_ENV", "DEV");
+	DEFINE ("PAYPAL_USER", "homemenus.inbox_api1.gmail.com");
+	DEFINE ("PAYPAL_PASSWORD", "C29KYXQ9RNCB6ZLR");
+	DEFINE ("PAYPAL_SIGNATURE", "AFcWxV21C7fd0v3bYYYRCpSSRl31AvgsNoywKGG.vIR-KOkSRKLCbGw2");
+	
+	/* STRIPE */
+	DEFINE("STRIPE_PUBLIC_KEY", "pk_test_M0qVCl3qzju6XEHOi5jIDWg8");
+	DEFINE("STRIPE_SECRET_KEY", "sk_test_lIqBT1ebwHGK3sN2xkHIX8kc");
 
 ?>
